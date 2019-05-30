2204bb588a7b021f5f857f143a0b62bbca2be7c6
joaowick <joaotorrez14@gmail.com>
2019-05-30 10:10:15 -0300

    ajuste table

--- pages/Como Ajudar/Albergue/albergue.js
+++ pages/Como Ajudar/Albergue/albergue.js
@@ -1,25 +1,16 @@
-function openBairro(BairroName) {
-    var i;
-    var x = document.getElementsByClassName("bairro");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    document.getElementById(BairroName).style.display = "block";
+function openCity(evt, regiaoNome) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(regiaoNome).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 
-function showPanel(panelIndex, colorCode) {
-    var tabButtons = document.querySelectorAll('.botaoPanel');
-    var tabPanels = document.querySelectorAll('.tabPanel');
-
-    tabButtons.forEach(function(node) {
-        node.style.backgroundColor = "";
-        node.style.color="";
-    });
-    tabButtons[panelIndex].style.backgroundColor = colorCode;
-    tabButtons[panelIndex].style.color="white";
-    tabPanels.forEach(function(node) {
-        node.style.display="none";
-    });
-    tabPanels[panelIndex].style.display="block";
-    tabPanels[panelIndex].style.backgroundColor=colorCode;
-}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
